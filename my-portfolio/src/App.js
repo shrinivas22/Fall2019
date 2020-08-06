a3c2f6bc9fdc4b57286e99afe3157ef5408db770
Shrinivas Suresh <shrisrinivas22@gmail.com>
2020-08-06 13:44:20 -0400

    my-portfolio 2nd commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,45 @@
+
+import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
+import resumeData from './res_data.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./styles/index.scss";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar resumeData={resumeData}/>
+        //<footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+///function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
